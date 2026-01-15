a4ec29ca44df60a63431b9ea8f0b373405b9583f
YairZen <zeneve.yair@gmail.com>
2026-01-15 16:32:06 +0200

    Added CSV Upload and Add student from list + model and API

---
+++ teaminsight/models/CsvStudentList.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const CsvStudentListSchema = new mongoose.Schema({
+  memberId: {
+    type: String,
+    required: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.models.CsvStudentList ||
+  mongoose.model("CsvStudentList", CsvStudentListSchema);
